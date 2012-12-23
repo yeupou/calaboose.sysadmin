c4b8d9eb6b92b6d1747aad7a98c3c60b8058421f
Klink <klink@bender>
2012-12-23 23:43:47 +0100

    Working version

--- index.pl
+++ index.pl
@@ -21,22 +21,60 @@ use strict;
 use DBI;
 use CGI qw(:standard Link);
 use CGI::Carp;
-use Net::Domain qw(hostname hostfqdn hostdomain domainname);
+use Net::Domain qw(hostdomain); 
+use URI::Encode qw(uri_encode);
+use Data::Password qw(:all);
 
-my $db_password = "DAnalkdnAD";
+my $db_password = "kdkadkda";
+my $wlan = "wlan1";
+my $wlan_deny = "/etc/hostapd/hostapd.deny";
+my $wlan_conf = "/etc/hostapd/hostapd.conf";
+
+# database connect
+my $dbd =DBI->connect('DBI:mysql:database=sysadmin:host=localhost',
+		      'www-data',
+		      $db_password,
+		      { RaiseError => 1, AutoCommit => 1});
+
+
+# update wifi related info
+if (param('client')) {
+    if (param('status')) {
+	# update the database without much checks 
+	$dbd->do("UPDATE wificlients SET status=".$dbd->quote(param('status'))." WHERE hw_address=".$dbd->quote(param('client'))); 
+    } elsif (param('forget')) {
+	# update the database without much checks
+	$dbd->do("DELETE FROM wificlients WHERE hw_address=".$dbd->quote(param('client')));
+    }
+    # reload the page so the url get cleaned up 
+    print redirect(script_name());
+}
+
 
 # get a list of users thatr belongs to group users
 my (@grnam) = getgrnam('users');
 my (@users) = split(' ',$grnam[3]);
 
+# find out their email alias
+my %users_plus_emails;
+open(ALIASES, "< /etc/aliases");
+while(<ALIASES>) {
+    my ($thisuser, $thisalias) = split(":", $_);
+    $thisalias =~ s/\s//g;
+    next unless ($thisuser ~~ @users);
+    $users_plus_emails{$thisuser} = "$thisuser <$thisalias>";
+}
+close(ALIASES);
 
-# start html
+# start html (using phpsysinfo theme)
 print header(-charset => 'UTF-8');
 print start_html(-lang =>  'fr-FR',
-                 -title => "Administration de ".hostdomain());
+                 -title => "Administration du domaine ".hostdomain(),
+		 -head => [ Link({-rel=>'stylesheet', -type=>'text/css', -href=>'/sysinfo/templates/two.css'})."\n" ] );
 
+print h1("Administration du domaine ".hostdomain()).br().br().br().br();
 
-# Reset password
+## RESET SAMBA/UNIX PASSWORD
 print h2("Utilisateurs :");
 
 if (param('user')) {
@@ -47,25 +85,103 @@ if (param('user')) {
 
     # feed the database - a distinct script will do the change, as it requires
     # root privileges
-    my $dbd =DBI->connect('DBI:mysql:database=updatepasswords:host=localhost',
-			  'www-data', 
-			  $db_password,
-			  { RaiseError => 1, AutoCommit => 1});
-    $dbd->do("INSERT INTO requests VALUES ('".$ENV{'REMOTE_ADDR'}."','".param('user')."')");
+    $dbd->do("REPLACE INTO sambaclients VALUES (".$dbd->quote($ENV{'REMOTE_ADDR'}).",".$dbd->quote(param('user')).")");
 
     print p("Demande de changement de mot de passe pour ".param('user')." enregistrée.");
 
-} else {
-    # Otherwise print the form
-    print start_form(-method=>"POST",-action=>script_name());
-    print "Réinitialiser le mot de passe de ";
-    print popup_menu(-name=>"user",-values=>\@users);
-    print submit().end_form();
-    print em("(Ne fonctionnera que si /etc/aliases est à jour)");
-    print end_html();
+} 
+# Always print the form anyway
+print start_form(-method=>"POST",-action=>script_name());
+print "Réinitialiser le mot de passe de ";
+print popup_menu(-name=>"user",-values=>\@users,-labels=>\%users_plus_emails);
+print submit().end_form();
+print br().em("(Ne fonctionnera que si /etc/aliases est à jour)");
+print end_html();
+
+# PRINT WIFI PASSWORD AND LIST OF WIFI CLIENTS 
+print br().br();
+print h2("Connexions wifi :");
+
+# find wifi pass
+open(WLAN_CONF, "< $wlan_conf");
+my $wlan_pass;
+while(<WLAN_CONF>) { $wlan_pass = $1 if /^wpa_passphrase=(.*)$/; }
+close (WLAN_CONF);
+print p("Le mot de passe wifi ".em("($wlan_conf)")." est $wlan_pass");
+# assess strenght
+$MINLEN = 50;
+$MAXLEN = 63;
+$GROUPS = 3;
+$FOLLOWING = 2;
+$FOLLOWING_KEYBOARD = 1;
+$DICTIONARY = 5;
+@DICTIONARIES = ("/usr/share/dict/french", "/usr/share/dict/american-english");
+
+my $failcheck = IsBadPassword($wlan_pass);
+if ($failcheck) {
+    # generate random example  
+    my @chars = (0 .. 9, 'a' .. 'z', 'A' .. 'Z', ',' .. '-');
+    my $random =  join "", @chars[ map rand @chars, 0 .. 55 ];
+    print p("Il est ".a({-href=>'http://en.wikipedia.org/wiki/Wi-Fi_Protected_Access#Security'}, "visiblement insécure")." : ".em($failcheck)).p("Exemple de mot de passe sécure : ".em($random));
+}
+print br();
+
+print h3("Clients autorisés :");
+
+# look at the database (updated by the cronjob)
+# to check the status of currently known clients
+# Is it possible then to:
+#    approve
+#    remove from list of approved
+
+my $hop;
+$hop = $dbd->prepare("SELECT * FROM wificlients WHERE status='A' OR status='S' or status='SI'");
+$hop->execute;
+my (%db_client_status);
+while (my ($client, $client_status, $client_ip, $client_name) = $hop->fetchrow_array) {
+    my $string;
+
+    # show a color code depending on the status
+    my $status_color = "#000000";
+    $status_color = "#1CA61E" if $client_status eq 'A';
+    $status_color = "#FFBC48" if $client_status eq 'S' or $client_status eq 'SI';
+    $string = span({-style=>"background-color: ".$status_color}, "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")."&nbsp;";
+
+    # show the most meaningful info to the end user if possible:
+    # hostname > ip > hwaddress 
+    if ($client_name) {
+	$string .= a({-title=>"$client_ip $client"}, $client_name);
+    } elsif ($client_ip) {
+	$string .= a({-title=>"$client"}, $client_ip);
+    } else {
+	$string .= $client;
+    }
+
+    $string .= ".................. ";
+    $string .= a({-href=>uri_encode(script_name()."?client=$client&amp;status=A")}, "confirmer")." / " 
+	if $client_status ne 'A';
+    $string .= a({-href=>uri_encode(script_name()."?client=$client&amp;forget=1")}, "oublier")." / ";
+    $string .= a({-href=>uri_encode(script_name()."?client=$client&amp;status=B")}, "bannir");
+
+    print p($string);
 }
 
 
-print h2("Wifi :");
+
+print br().h3("Clients bannis ".em("($wlan_deny)")." :");
+open(WLAN_DENY, "< $wlan_deny");
+my $deny;
+while(<WLAN_DENY>) {
+    chomp();
+    $deny .= " $_,";
+}
+close(WLAN_DENY);
+chop($deny);
+$deny = "Aucun" unless $deny;
+print p(span({-style=>"background-color: #7A090B"}, "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")."&nbsp;".$deny);
+close(WLAN_DENY);
+
+
+
 
 # EOF
